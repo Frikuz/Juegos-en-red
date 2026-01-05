1025d1bfdf8db8416059f0a8e6dd35bc921bacea
Frikuz <166073909+Frikuz@users.noreply.github.com>
2026-01-05 21:26:47 +0100

    Música, historia y pequeños detalles
    Se ha implementado música para el menú y el juego, además de añadir la historia y arreglar errores mínimos de formato y bugs

---
+++ src/entities/Car.js
@@ -0,0 +1,54 @@
+import Phaser from "phaser";
+
+export class Car extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, id, x, y, texture) {
+    super(scene, x, y, texture);
+
+this.laps = 0;
+this.passedCheckpoint = false;
+this.justCrossedFinish = false;
+
+    this.id = id;
+    this.scene = scene;
+
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+
+    // VELOCIDADES
+    this.baseSpeed = 300;
+    this.offRoadSpeed = 120;
+    this.currentSpeed = this.baseSpeed;
+
+    this.isOffRoad = false;
+
+    // CONFIG FÍSICA
+    this.setCollideWorldBounds(true);
+    this.body.allowGravity = false;
+    this.setOrigin(0.5);
+    this.setDrag(400, 400);
+
+    this.setScale(0.4);
+
+this.body.setSize(
+  this.width * 0.6,
+  this.height * 0.8
+);
+
+this.body.setOffset(
+  this.width * 0.2,
+  this.height * 0.1
+);
+    
+  }
+
+  setOffRoad(value) {
+
+    if (this.isOffRoad === value) return;
+    this.isOffRoad = value;
+    this.currentSpeed = value ? this.currentSpeed-=180 : this.currentSpeed+=180;
+
+    // Feedback visual opcional
+    this.setTint(value ? 0xaaaaaa : 0xffffff);
+  }
+
+}
---
+++ src/scenes/PauseScene.js
@@ -0,0 +1,48 @@
+import Phaser from "phaser";
+
+export class PauseScene extends Phaser.Scene {
+  constructor() {
+    super("PauseScene");
+  }
+
+  create(data) {
+    this.add.rectangle(640, 480, 1280, 960, 0x000000, 0.7);
+
+    this.add
+      .text(640, 320, "Juego en pausa", {
+        fontSize: "48px",
+        color: "#ffffff"
+      })
+      .setOrigin(0.5);
+
+    const resumeBtn = this.add
+      .text(640, 512, "Reanudar", {
+        fontSize: "32px",
+        color: "#00ff00"
+      })
+      .setOrigin(0.5)
+      .setInteractive({ useHandCursor: true })
+      .on("pointerover", () => resumeBtn.setColor("#00ff88"))
+      .on("pointerout", () => resumeBtn.setColor("#00ff00"))
+      .on("pointerdown", () => {
+        this.scene.stop();
+        this.scene.resume(data.originalScene);
+        this.scene.get(data.originalScene).resume();
+      });
+
+    const menuBtn = this.add
+      .text(640, 640, "Volver al menú principal", {
+        fontSize: "32px",
+        color: "#ffffff"
+      })
+      .setOrigin(0.5)
+      .setInteractive({ useHandCursor: true })
+      .on("pointerover", () => menuBtn.setColor("#ff8888"))
+      .on("pointerout", () => menuBtn.setColor("#ffffff"))
+      .on("pointerdown", () => {
+        this.sound.stopAll();
+        this.scene.stop(data.originalScene);
+        this.scene.start("MenuScene");
+      });
+  }
+}
